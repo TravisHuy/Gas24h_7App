85a14f4a1b79d339635e3f0404f9a0f0509d8b94
TravisHuy <honhathuy098@gmail.com>
2024-08-21 14:08:11 +0700

    update xml order

---
+++ app/src/main/java/com/nhathuy/gas24h_7app/ui/order/OrderActivity.kt
@@ -0,0 +1,12 @@
+package com.nhathuy.gas24h_7app.ui.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nhathuy.gas24h_7app.R
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+}
---
+++ app/src/main/java/com/nhathuy/gas24h_7app/ui/order/OrderContract.kt
@@ -0,0 +1,4 @@
+package com.nhathuy.gas24h_7app.ui.order
+
+interface OrderContract {
+}
